e16bcbe5120f7e188fc98c5ed8f9e4fab0f67a71
Marcel Mulders <marcel@dotfive.co.uk>
2021-07-08 23:52:53 +0100

    finished game loop for battling

--- js-gladiator/assets/js/game.js
+++ js-gladiator/assets/js/game.js
@@ -1,6 +1,13 @@
 /* Game code here */
 
 // Set local variables
+
+let activeWindowOption = [
+    "battle-dialogue",
+    "attack-selection",
+    "win-dialogue",
+];
+let activeWindow = "battle-dialogue";
 let runAnimation = true;
 
 let attacks = {
@@ -29,6 +36,23 @@ let attacks = {
 let activeAttack = 1;
 let opponentHP;
 let playerHP;
+let playerXP = 0;
+let playerLevel = 1;
+
+let opponentNames = [
+    "Achilleas",
+    "Adam",
+    "Agapi",
+    "Aimilios",
+    "Charalampos",
+    "Charilaos",
+    "Christos",
+    "Demetrios",
+    "Diamanto",
+    "Dimitris",
+    "Dorotheos",
+    "Efthymios",
+];
 
 // General functions
 function sleep(ms) {
@@ -37,6 +61,8 @@ function sleep(ms) {
 
 // Game load function
 function setup() {
+
+    document.getElementById("dialogue-text").innerHTML = "Gor is challenging you to a battle. Beat Gor for the glory of Greece!";
 
     // Up Button
     document.getElementById("game-nav-vertical-1").addEventListener("click", async function() {
@@ -81,7 +107,33 @@ function setup() {
     // A Button
     document.getElementById("A-button").addEventListener("click", function() {
         console.log("A PRESS");
-        submitAttack();
+        console.log(activeWindow);
+        if (activeWindow == "battle-dialogue") {
+            // Set total HP
+            opponentHP = 10
+            document.getElementById("opponent-hp").className = "nes-progress is-success";
+            document.getElementById("opponent-hp").value = opponentHP;
+            document.getElementById("opponent-hp").max = opponentHP;
+            playerHP = 10;
+            document.getElementById("player-hp").max = playerHP;
+
+            // Setup player XP
+            document.getElementById("player-xp").max = playerLevel * 15;
+            document.getElementById("player-xp").value = playerXP;
+
+            // Show attack menu
+            document.getElementById("battle-dialogue-section").style.display = "none";
+            document.getElementById("attack-selection-menu").style.display = "block";
+            activeWindow = "attack-selection";
+            // start the game base animation
+            startBattle();
+        } else if (activeWindow == "win-dialogue") {
+            let namePick = parseInt( Math.random() * opponentNames.length );
+            document.getElementById("dialogue-text").innerHTML = opponentNames[namePick] + " is challenging you to a battle. Beat " + opponentNames[namePick] + " for the glory of Greece!";
+            activeWindow = "battle-dialogue";
+        } else if (activeWindow == "attack-selection") {
+            submitAttack();
+        }
     });
 
     // B Button
@@ -89,14 +141,8 @@ function setup() {
         console.log("B PRESS");
     });
 
-    // Set total HP
-    opponentHP = 10
-    document.getElementById("opponent-hp").max = opponentHP;
-    playerHP = 10;
-    document.getElementById("player-hp").max = playerHP;
-
-    startBattle();
-
+    
+    
     // testing
     
 
@@ -125,6 +171,9 @@ async function submitAttack() {
 }
 
 async function calculateIfDead() {
+    document.getElementById("game-stats").className = "m-2";
+    document.getElementById("game-view").className = "m-2";
+
     if (opponentHP <= 0) {
         runAnimation = false;
         await sleep(500);
@@ -154,10 +203,39 @@ async function calculateIfDead() {
             await sleep(reloadRate);
                     
         }
+
+        await sleep(2000);
+
+        playerXP = playerXP + parseInt( document.getElementById("opponent-level").innerHTML ) * 2;
+        document.getElementById("player-xp").value = playerXP;
+
+        await sleep(2000);
+
+        activeWindow = "win-dialogue";
+        document.getElementById("attack-selection-menu").style.display = "none";
+        document.getElementById("dialogue-text").innerHTML = "Congratulations, you have destroyed your opponent!";
+        document.getElementById("battle-dialogue-section").style.display = "block";
+
+        document.getElementById("game-stats-placeholder").style.display = "block";
+        document.getElementById("game-stats").style.display = "none";
+
+        document.getElementById("game-view-placeholder").style.display = "block";
+        document.getElementById("game-view").style.display = "none";
+
+        runAnimation = true;
     }
 }
 
 async function startBattle() {
+
+    document.getElementById("game-stats-placeholder").style.display = "none";
+    document.getElementById("game-stats").className = "m-2 animate__animated animate__fadeInLeft";
+    document.getElementById("game-stats").style.display = "block";
+
+    document.getElementById("game-view-placeholder").style.display = "none";
+    document.getElementById("game-view").className = "m-2 animate__animated animate__fadeInLeft";
+    document.getElementById("game-view").style.display = "block";
+
     let canvas = document.getElementById("game-canvas");
     let context = canvas.getContext("2d");
     let image = document.getElementById("gladiator-opponent");
